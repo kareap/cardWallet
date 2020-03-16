ac1fbd33d0ab39fcace37846a1b17573236d9c85
Rina <rina.alickaj@hotmail.com>
2020-03-16 17:08:59 +0100

    H2 database issue #1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,23 @@
-INSERT INTO USER_DATA(email,password)
-VALUES ('olav.nordmann@hotmail.com', '1234');
+INSERT INTO USER ()
+VALUES ();
 
-INSERT INTO USER_DATA(email, password)
-VALUES ('kari.nordmann@hotmail.com', '1234');
+INSERT INTO USER ()
+VALUES ();
 
-INSERT INTO USER_DATA(email, password)
-VALUES ('nils.nordmann@hotmail.com', '1234');
+INSERT INTO USER ()
+VALUES ();
 
-INSERT INTO GIFT_CARD(store_name, card_code, balance, user_id)
-VALUES ('H&M', '12345678', 500, 1);
+INSERT INTO USER_DATA(email,password, user_id)
+VALUES ('olav.nordmann@hotmail.com', '1234', 1);
+
+INSERT INTO USER_DATA(email, password, user_id)
+VALUES ('kari.nordmann@hotmail.com', '1234', 2);
+
+INSERT INTO USER_DATA(email, password, user_id)
+VALUES ('nils.nordmann@hotmail.com', '1234', 3);
+
+INSERT INTO GIFT_CARD(store_name, card_code, balance)
+VALUES ('H&M', '12345678', 500);
 
 INSERT INTO GIFT_CARD(store_name, card_code, balance, user_id)
 VALUES ('H&M', '87654321', 700, 2);
