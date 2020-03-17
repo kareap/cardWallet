e0e694918245dd5f85809933d0d7d876ed11dcac
KarolineE <eap-k@hotmail.com>
2020-03-17 11:19:28 +0100

    add getAllCards method

--- src/main/java/no/cardwallet/card/GiftCard/GiftCardController.java
+++ src/main/java/no/cardwallet/card/GiftCard/GiftCardController.java
@@ -1,12 +1,23 @@
 package no.cardwallet.card.GiftCard;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class GiftCardController {
 
+    @Autowired
+    GiftCardRepository giftCardRepository;
+
+    @GetMapping("/")
+    public String getAllCards() {
+        List<GiftCard> giftCardList = (List<GiftCard>) giftCardRepository.findAll();
+return null;
+    }
 
 
 
