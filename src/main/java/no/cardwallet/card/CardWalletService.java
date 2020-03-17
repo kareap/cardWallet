48d3957912d4a43c55281c51261b2f93a6d3981c
marianne-ott <mar.ottersen@gmail.com>
2020-03-17 10:58:26 +0100

    comments deleted

--- src/main/java/no/cardwallet/card/CardWalletService.java
+++ src/main/java/no/cardwallet/card/CardWalletService.java
@@ -1,17 +1,8 @@
 package no.cardwallet.card;
-
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class CardWalletService {
-
-//    private final AppUserRepository appUserRepository;
-//
-//    public CardWalletService(AppUserRepository appUserRepository) {
-//        this.appUserRepository = appUserRepository;
-//    }
-//
 
 
 }
