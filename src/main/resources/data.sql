34239fac9ce29bf147afdee2fcfcd247bf900134
KarolineE <eap-k@hotmail.com>
2020-03-24 16:27:23 +0100

    changed giftcards in h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,14 +8,14 @@ INSERT INTO app_user (email, password)
 VALUES ('nils.nordmann@hotmail.com', '$2a$10$.TfieO7U8dAd3v4IXpIr8.5ltZJLFHc1NFAMUcC51VuZqY1WHq932');
 
 INSERT INTO gift_card (store_name, card_code, balance_int, app_user_id, expiry_date, logo_image)
-VALUES ('H&M', '12345678', 50050, 1, TO_DATE('22.12.2020', 'DD/MM/YYYY'), 'Logo image URL');
+VALUES ('Jernia', '12345678', 50050, 1, TO_DATE('22.12.2020', 'DD/MM/YYYY'), 'Logo image URL');
 
 INSERT INTO gift_card (store_name, card_code, balance_int, app_user_id, expiry_date, logo_image)
 VALUES ('Clas Ohlson', '12345543', 82299, 1, TO_DATE('02/02/2022', 'dd/mm/yyyy'), '(Insert image URL here)');
 
 INSERT INTO gift_card (store_name, card_code, balance_int, app_user_id, expiry_date, logo_image)
-VALUES ('Cubus', '9934356738', 90000, 1, TO_DATE('13/05/2022', 'dd/mm/yyyy'), '(Insert image URL here)');
+VALUES ('Intersport', '9934356738', 90000, 1, TO_DATE('13/05/2022', 'dd/mm/yyyy'), '(Insert image URL here)');
 
 INSERT INTO gift_card (store_name, card_code, balance_int, app_user_id, expiry_date, logo_image)
-VALUES ('Jernia', '87654321', 70000, 2, TO_DATE('19/09/2021', 'dd/mm/yyyy'), '(Insert image URL here)');
+VALUES ('Cubus', '87654321', 70000, 2, TO_DATE('19/09/2021', 'dd/mm/yyyy'), '(Insert image URL here)');
 
